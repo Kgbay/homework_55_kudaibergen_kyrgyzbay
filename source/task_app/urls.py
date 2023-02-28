96269ee571f3b9683a5f2963da1b15a8c3ac5286
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-02-28 10:27:05 +0600

    Добавил пути удаление и изменение данных

--- source/task_app/urls.py
+++ source/task_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views.base import index_view
-from .views.tasks import add_view, detail_view, remove_view
+from .views.tasks import add_view, detail_view, remove_view, update_view, confirm_delete_view
 
 urlpatterns = [
     path("", index_view, name='index'),
     path("task/add/", add_view, name="task_add"),
     path('task/<int:pk>', detail_view, name='detail_view'),
-    path("remove/<int:pk>", remove_view, name='remove_view')
+    path("remove/<int:pk>", remove_view, name='remove_view'),
+    path("task/<int:pk>/confirm_delete", confirm_delete_view, name='confirm_delete'),
+    path("task/<int:pk>/update/", update_view, name='task_update')
 ]
